d54fe1f804a128e4370fa356be02b1ddbe6d8442
Viswanathan Devarajan <viswanathanlakshmi@ViswanathansMBP.home>
2019-05-15 21:48:56 -0400

    change of server url

--- src/components/Api.js
+++ src/components/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosService = () => {
   return axios.create({
-    baseURL: `http://localhost:8081`
+    baseURL: `https://vue-react-server.herokuapp.com`
   })
 }
 export default {
