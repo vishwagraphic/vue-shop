f1e164df6b060a43eb68ebebe227a2e972ff0d42
Viswanathan Devarajan <viswanathanlakshmi@ViswanathansMBP.home>
2019-05-16 14:51:39 -0400

    modified

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,7 @@ export default {
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  -webkit-font-smoothing: antialiased; 
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
