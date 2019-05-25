fed49ea89316ef1d6882ecc95526d00da1482b50
Viswanathan Devarajan <viswanathanlakshmi@ViswanathansMBP.home>
2019-05-24 21:05:21 -0400

    API url change

--- src/components/Api.js
+++ src/components/Api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const axiosService = () => {
   return axios.create({
+    // baseURL: `http://localhost:8081`
     baseURL: `https://vue-react-server.herokuapp.com`
   })
 }
